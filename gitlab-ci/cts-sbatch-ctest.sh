468fcbb6e1ab86ea094ebf30233295ea0a6f1fcc
Gary L. Hennigan <glhenni@sandia.gov>
2026-01-08 14:15:21 -0700

    add automatic determination of cores for running simultaneous tests

--- gitlab-ci/cts-sbatch-ctest.sh
+++ gitlab-ci/cts-sbatch-ctest.sh
@@ -14,31 +14,10 @@ cleanInstallDir() {
 ## sbatch. For salloc these must be specified on the command line.
 nodes=$SLURM_JOB_NUM_NODES          # Number of nodes
 
-# Number MPI processes to run on each node (a.k.a. PPN)
-#
-# the multiplier needs to be the smallest number of cores per node on
-# the list of cts machines being used for testing. current list of
-# cores per node on the relevant machines is:
-#    amber|flight|hops: 112
-#    manzano|jetcool: 48
-#    attaway|ghost: 36
-case ${SLURM_CLUSTER_NAME} in
-  ghost|attaway|eclipse)
-    cmult=36
-    ;;
-  amber|flight|hops)
-    cmult=112
-    ;;
-  manzano|jetcool)
-    cmult=48
-    ;;
-  *)
-    cmult=12
-    ;;
-esac
-
+physCoreCount=$(lscpu -p | grep -v '^#' | sort -u -t, -k 2,4 | wc -l)
 # divide by two assuming xyce jobs are using "mpirun -np 2 ..."
-cores=$(( ${nodes}*${cmult}/2 ))
+coreCount=$(( physCoreCount/2 ))
+
 rawCmakeArgsList="$1"
 xyceInstallDir="$2"
 CI_PROJECT_DIR="$3"
@@ -49,7 +28,7 @@ export PATH
 # note use of the use of the pipeline's xyce-ctest.cmake file, NOT the
 # build repos copy
 ctest --timeout 1200 -DVERBOSITY=5 \
-  -DNUM_PROCS=28 \
+  -DNUM_PROCS=${coreCount} \
   -DUSE_GITLAB_CI_TESTING=ON \
   -DCMAKE_ARGS_LIST="${rawCmakeArgsList}" \
   -S ${CI_PROJECT_DIR}/cmake/ctest/xyce-ctest.cmake
